d911565f8e52f2f2082d4116d0d2f83527ab364c
unknown <you@example.com>
2018-07-24 18:09:27 +0430

    individual id get

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -29,6 +30,23 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+   let id = req.params.id;
+   if (!ObjectId.isValid(id)) {
+       return res.status(404).send();
+   }
+
+   Todo.findById(id).then((todo) => {
+       if (!todo) {
+           return res.status(404).send();
+       }
+
+       res.send({todo, name: 'rasekh moafi'});
+   }).catch((e) => {
+       res.status(400).send();
+   });
 });
 
 
